21968b4577c0775524306de81166a6ebaa1016e1
Linuxea <Linuxea@foxmail.com>
2017-11-25 14:32:56 +0800

    实例对象的缓存用来提高

--- src/main/java/com/linuxea/mvc/DispatcherServlet.java
+++ src/main/java/com/linuxea/mvc/DispatcherServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,6 +23,16 @@ import java.util.Map;
 public class DispatcherServlet extends HttpServlet {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    /**
+     * 用来缓存 method 对象的实例
+     */
+    private static final Map<Class, Method> MAP = new HashMap<>();
+
+    /**
+     * 对象映射关系的获取
+     */
+    private static final Map<String, Class<? extends Method>> controllerMappingMap = ControllerConfig.getMap();
 
     public DispatcherServlet() {
         // only init once
@@ -34,12 +45,16 @@ public class DispatcherServlet extends HttpServlet {
         HttpServletRequest httpServletRequest = (HttpServletRequest) req;
         HttpServletResponse httpServletResponse = (HttpServletResponse) res;
         String uri = httpServletRequest.getRequestURI().toString();
-        Map<String, Class<? extends Method>> map = ControllerConfig.getMap();
         Method method;
         try {
-            Class<? extends Method> inst = map.get(uri);
+            Class<? extends Method> inst = controllerMappingMap.get(uri);
             if (null != inst) {
-                method = inst.newInstance();
+                if (null != MAP.get(inst)) {
+                    method = MAP.get(inst);
+                } else {
+                    method = inst.newInstance();
+                    MAP.put(inst, method);
+                }
                 method.validateType(httpServletRequest, httpServletResponse);
                 method.doIt(httpServletRequest, httpServletResponse);
             } else {
